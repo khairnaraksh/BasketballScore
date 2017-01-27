7334ae00997234ee957eea4caf6eb84877487966
Akshay <khairnaraksh@gmail.com>
2017-01-27 19:52:33 +0530

    Update Project

--- app/src/main/java/com/example/mamta/court_counter/MainActivity.java
+++ app/src/main/java/com/example/mamta/court_counter/MainActivity.java
@@ -6,7 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int scoreteamA=0;
+    int scoreteamA = 0;
+    int scoreteamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,22 +16,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void threepoints(View view) {
-        displayForTeamA(scoreteamA=scoreteamA+3);
+        displayForTeamA(scoreteamA = scoreteamA + 3);
 
     }
 
     public void twopoint(View view) {
-        displayForTeamA(scoreteamA=scoreteamA+2);
+        displayForTeamA(scoreteamA = scoreteamA + 2);
 
     }
 
     public void freepoint(View view) {
-        displayForTeamA(scoreteamA=scoreteamA+1);
+        displayForTeamA(scoreteamA = scoreteamA + 1);
 
     }
 
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.point_textView);
+        TextView scoreView = (TextView) findViewById(R.id.point_textViewA);
         scoreView.setText(String.valueOf(score));
+    }
+
+
+    public void threepointsB(View view) {
+        displayForTeamB(scoreteamB = scoreteamB + 3);
+    }
+
+    public void twopointB(View view) {
+        displayForTeamB(scoreteamB = scoreteamB + 2);
+
+    }
+
+    public void freepointB(View view) {
+        displayForTeamB(scoreteamB = scoreteamB + 1);
+
+    }
+
+    public void displayForTeamB(int score1) {
+        TextView scoreView = (TextView) findViewById(R.id.point_textViewB);
+        scoreView.setText(String.valueOf(score1));
+    }
+
+    public void resetscore(View view) {
+        scoreteamA = 0;
+        scoreteamB = 0;
+        displayForTeamA(scoreteamA);
+        displayForTeamB(scoreteamB);
     }
 }
